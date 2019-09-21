1d725daaa675693741ffd10bcfa8f2e9b224891c
Kelvin Huang <51042844+hanyun2019@users.noreply.github.com>
2019-09-21 18:47:53 +0800

    Update MicroService.py

--- Part-One/MicroService.py
+++ Part-One/MicroService.py
@@ -7,7 +7,8 @@
 #
 #
 
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
+from flask import make_response
 import json
 import sqlite3
 
@@ -46,6 +47,44 @@ def list_user(user_id):
     conn.close()
     return jsonify(a_dict)
 
+def add_user(new_user):
+    conn = sqlite3.connect('mydb.db')
+    print ("Open database mydb.db successfully");
+    api_list=[]
+    cursor=conn.cursor()
+    cursor.execute("select * from users where username=? or emailid=?", (new_user['username'], new_user['email']))
+    data = cursor.fetchall()
+    if len(data) != 0:
+        abort(409)
+    else:
+        cursor.execute("insert into users (username, emailid, password, full_name) values(?,?,?,?)", (new_user['username'], new_user['email'], new_user['password'], new_user['name']))
+        conn.commit()
+        return "Success"
+    conn.close()
+    return jsonify(a_dict)
+
+#   Use API to DEBUG:
+#   curl -i -H "Content-Type: application/json" -X POST -d '{"username":"michael123", "email": "MichaelJackson@horizonit.com", "password": "mk12345", "name": "Michael Jackson"}' http://localhost:5000/api/v1/users
+#   curl -i -H "Content-Type: application/json" -X POST -d '{"username":"alanwalker", "email": "AlanWalker@horizonit.com", "password": "aw12345", "name": "Alan Walker"}' http://localhost:5000/api/v1/users
+#
+
+def del_user(del_user):
+    conn = sqlite3.connect('mydb.db')
+    print ("Open database mydb.db successfully");
+    cursor=conn.cursor()
+    cursor.execute("select * from users where username=? ", (del_user,))
+    data = cursor.fetchall()
+    print("Data", data)
+    if len(data) == 0:
+        abort(404)
+    else:
+        cursor.execute("delete from users where username==?", (del_user,))
+    conn.commit()
+    return "Success"
+
+#   Use API to DEBUG:
+#   curl -i -H "Content-Type: application/json" -X delete -d '{"username":"michael123"}' http://localhost:5000/api/v1/users
+#
 
 
 @app.route("/api/v1/info")
@@ -53,7 +92,7 @@ def home_index():
     conn = sqlite3.connect('mydb.db')
     print ("Open database mydb.db successfully");
     api_list=[]
-    cursor = conn.execute("SELECT buildtime, version, methods, links from apirelease")
+    cursor = conn.execute("select buildtime, version, methods, links from apirelease")
     for row in cursor:
         api = {}
         api['version'] = row[0]
@@ -64,7 +103,6 @@ def home_index():
     conn.close()
     return jsonify({'api_version': api_list}), 200
 
-
 @app.route('/api/v1/users', methods=['GET'])
 def get_users():
     return list_users()
@@ -73,7 +111,34 @@ def get_users():
 def get_user(user_id):
     return list_user(user_id)
 
+@app.route('/api/v1/users', methods=['POST'])
+def create_user():
+    if not request.json or not 'username' in request.json or not 'email' in request.json or not 'password' in request.json:
+        abort(400)
+    user = {
+        'username': request.json['username'],
+        'email': request.json['email'],
+        'name': request.json.get('name', ""),
+        'password': request.json['password']
+    }
+    return jsonify({'status': add_user(user)}), 201
+
+@app.route('/api/v1/users', methods=['DELETE'])
+def delete_user():
+    if not request.json or not 'username' in request.json: abort(400)
+    user=request.json['username']
+    return jsonify({'status': del_user(user)}), 200
+
+
+@app.errorhandler(404)
+def resource_not_found(error):
+    return make_response(jsonify({'error': 'Resource not found.'}), 404)
+
+@app.errorhandler(400)
+def invalid_request(error):
+    return make_response(jsonify({'error': 'Bad Request'}), 400)
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
+
